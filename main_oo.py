c5056d114b73d4badf5cb04127f0adabcaa96588
Aleksander Wójtowicz <a.wojtowicz@avsystem.com>
2021-10-24 11:38:15 +0200

    restructure scanner_oo.py

--- main_oo.py
+++ main_oo.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 
-#object oriented version
-
 import sys
-import scanner_oo # scanner_oo.py is a file you create, (it is not an external library)
-
+import scanner_oo
 
 if __name__ == '__main__':
-
     try:
-        filename = sys.argv[1] if len(sys.argv) > 1 else "example2.txt"
+        filename = sys.argv[1] if len(sys.argv) > 1 else "example.txt"
         file = open(filename, "r")
     except IOError:
-        print("Cannot open {0} file".format(filename))
-        sys.exit(0)
+        print(f"Cannot open {filename} file")
+        sys.exit(1)
 
     text = file.read()
     lexer = scanner_oo.Scanner()
     lexer.build()
 
-    
     # Give the lexer some input
     lexer.input(text)
 
@@ -29,5 +24,3 @@ if __name__ == '__main__':
         if not tok: 
             break      # No more input
         print(f"({tok.lineno}): {tok.type}({tok.value})")
-        # print(tok)
-
--- scanner_oo.py
+++ scanner_oo.py
@@ -1,7 +1,6 @@
 import ply.lex as lex
 import sys
 
-# https://github.com/PrzemekBurczyk/PLY/blob/master/scanner.py
 class Scanner():
     t_ignore = " \t"
     literals = "+-*/=<>()[]{}:',;"
@@ -39,32 +38,6 @@ class Scanner():
         "STR"
     ] + list(_reserved.values())
 
-    def t_error(self, t: lex.LexToken):
-        print(f"Illegal character {t.value[0]} at line {t.lexer.lineno}", file=sys.stderr)
-
-        t.lexer.skip(1)
-
-    def t_ID(self, t):
-        r"[a-zA-Z_]\w*"
-        t.type = self._reserved.get(t.value, "ID")
-        return t
-
-    def t_newline(self, t):
-        r"\n+"
-        t.lexer.lineno += len(t.value)
-
-    def t_COMMENT(self, t):
-        r"\#.*"
-
-    def build(self):
-        self.lexer = lex.lex(object=self)
-
-    def input(self, text):
-        self.lexer.input(text)
-
-    def token(self):
-        return self.lexer.token()
-
     t_DOTADD = r"\.\+"
     t_DOTSUB = r"\.-"
     t_DOTMUL = r"\.\*"
@@ -80,3 +53,29 @@ class Scanner():
     t_STR = r"\".*\""
     t_INTNUM = r"\d+"
     t_FLOATNUM = r"[+-]?(\d+[.](\d*)?([eE][+-]?\d+)?|[.]\d+([eE][+-]?\d+)?)"
+
+    def t_ID(self, t):
+        r"[a-zA-Z_]\w*"
+        t.type = self._reserved.get(t.value, "ID")
+        return t
+
+    def t_COMMENT(self, t):
+        r"\#.*"
+
+    def t_newline(self, t):
+        r"\n+"
+        t.lexer.lineno += len(t.value)
+
+    def t_error(self, t):
+        print(f"Illegal character {t.value[0]} at line {t.lexer.lineno}", file=sys.stderr)
+
+        t.lexer.skip(1)
+
+    def build(self):
+        self.lexer = lex.lex(object=self)
+
+    def input(self, text):
+        self.lexer.input(text)
+
+    def token(self):
+        return self.lexer.token()
